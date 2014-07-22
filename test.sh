35d6e9b506c829fb5b8d1b41727bec1ced5cb5d7
Rui Campos <rui.campos@gameduell.de>
2014-07-22 14:25:08 +0200

    forgot to change one port

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ cd test
 haxelib run lime test html5 &
 cd ..
 
-python test_result_listener.py
+python test_result_listener.py 8181
 WEBSERVER_PROCESS_ID=`ps aux | grep http-server | grep -v grep | awk '{print $2}'`
 kill $WEBSERVER_PROCESS_ID
 
