ed58d482fef8523229bfa50f84349cddf87e0360
Rui Campos <rui.campos@gameduell.de>
2014-07-24 12:03:38 +0200

    wrong url

--- unittest/implementations/TestHTTPLogger.hx
+++ unittest/implementations/TestHTTPLogger.hx
@@ -20,9 +20,9 @@ class TestHTTPLogger implements unittest.TestLogger
     private var url : String;
 
 #if android
-    static public var DEFAULT_URL = "http://localhost:8181";
-#else
     static public var DEFAULT_URL = "http://10.0.2.2:8181";
+#else
+    static public var DEFAULT_URL = "http://localhost:8181";
 #end
 
     public function new(testLogger : TestLogger, url : String = null) : Void
