ddf419f8133a0ff43cb9fede4fd87697f5c1f404
kgar <khaled.garbaya@gamduell.de>
2014-11-04 14:43:43 +0100

    Fix flash tracing now in the same tf as haxe debug traces

--- unittest/Utils.hx
+++ unittest/Utils.hx
@@ -5,37 +5,15 @@ class Utils
 {
 
 
-    #if flash9
+    #if flash
         static var tf : flash.text.TextField = null;
-    #elseif flash
-        static var tf : flash.TextField = null;
     #end
 	static public dynamic function print(v : Dynamic) untyped
 	{
-		#if flash9
-			if( tf == null ) {
-				tf = new flash.text.TextField();
-				tf.selectable = false;
-				tf.width = flash.Lib.current.stage.stageWidth;
-				tf.autoSize = flash.text.TextFieldAutoSize.LEFT;
-				flash.Lib.current.addChild(tf);
-			}
-			tf.appendText(v);
-		#elseif flash
-			var root = flash.Lib.current;
-			if( tf == null ) {
-				root.createTextField("__tf",1048500,0,0,flash.Stage.width,flash.Stage.height+30);
-				tf = root.__tf;
-				tf.selectable = false;
-				tf.wordWrap = true;
-			}
+		#if flash
+            tf = flash.Boot.getTrace();
 			var s = flash.Boot.__string_rec(v,"");
-			tf.text += s;
-			while( tf.textHeight > flash.Stage.height ) {
-				var lines = tf.text.split("\r");
-				lines.shift();
-				tf.text = lines.join("\n");
-			}
+			tf.text +=s;
 		#elseif neko
 			__dollar__print(v);
 		#elseif php
