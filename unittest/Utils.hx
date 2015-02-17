e0444ecda41a6b20af9acd76fffe0e501b1fa757
Rui Campos <rui.campos@gameduell.de>
2015-02-17 19:28:37 +0100

    updated dependencies, now printing with the dulcet

--- unittest/Utils.hx
+++ unittest/Utils.hx
@@ -1,46 +1,12 @@
 package unittest;
 
+import duell.DuellKit;
 
 class Utils
 {
 
-
-    #if flash
-        static var tf : flash.text.TextField = null;
-    #end
 	static public dynamic function print(v : Dynamic) untyped
 	{
-		#if flash
-            tf = flash.Boot.getTrace();
-			var s = flash.Boot.__string_rec(v,"");
-			tf.text +=s;
-		#elseif neko
-			__dollar__print(v);
-		#elseif php
-			php.Lib.print(v);
-		#elseif cpp
-			cpp.Lib.print(v);
-		#elseif js
-			var msg = js.Boot.__string_rec(v,"");
-			var d;
-            if( __js__("typeof")(document) != "undefined"
-                    && (d = document.getElementById("haxe:trace")) != null ) {
-                msg = msg.split("\n").join("<br/>");
-                d.innerHTML += StringTools.htmlEscape(msg)+"<br/>";
-            }
-			else if (  __js__("typeof process") != "undefined"
-					&& __js__("process").stdout != null
-					&& __js__("process").stdout.write != null)
-				__js__("process").stdout.write(msg); // node
-			else if (  __js__("typeof console") != "undefined"
-					&& __js__("console").log != null )
-				__js__("console").log(msg); // document-less js (which may include a line break)
-
-		#elseif cs
-			cs.system.Console.Write(v);
-		#elseif java
-			var str:String = v;
-			untyped __java__("java.lang.System.out.print(str)");
-		#end
+		DuellKit.print(v);
 	}
 }
