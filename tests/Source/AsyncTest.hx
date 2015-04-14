89e228f08655864a8c91902cde61cb8248cb6b91
Rui Campos <rui.campos@gameduell.de>
2015-04-14 16:15:22 +0200

    updated the unit tests

--- tests/Source/AsyncTest.hx
+++ tests/Source/AsyncTest.hx
@@ -7,7 +7,7 @@
 
 import duell.DuellKit;
 
-import asyncrunner.DelayedTask;
+import asyncrunner.Async;
 import asyncrunner.FunctionTask;
 
 class AsyncTest extends unittest.TestCase
@@ -16,16 +16,16 @@ class AsyncTest extends unittest.TestCase
     {
         assertEquals("string", "string");
 
-        DelayedTask.delay(function() assertAsyncFinish(test1), 0.5);
+        Async.delay(function() assertAsyncFinish("test1"), 0.5);
 
-        assertAsyncStart(test1);
+        assertAsyncStart("test1");
     }
 
     public function test2()
     {
-        assertAsyncStart(test2, 0.1);
+        assertAsyncStart("test2", 0.1);
 
-        DelayedTask.delay(function() assertAsyncFinish(test2), 0.5);
+        Async.delay(function() assertAsyncFinish("test2"), 0.5);
 
         assertShouldFail();
         ///should timeout
@@ -34,9 +34,9 @@ class AsyncTest extends unittest.TestCase
     public function test3()
     {
         ///after 500ms, test2 timeout should happen
-        assertAsyncStart(test3, 2);
+        assertAsyncStart("test3", 2);
 
-        DelayedTask.delay(function() assertAsyncFinish(test3), 1.0);
+        Async.delay(function() assertAsyncFinish("test3"), 1.0);
     }
 
 
