358146e351ce00d9b43c17e5cf52636b8231c012
Christian Lührs <christian.luehrs@gameduell.de>
2015-11-25 12:29:35 +0100

    - implemented plugin.xml to get some output if you run -help - added RunMain

---
+++ duell/run/main/RunMain.hx
@@ -0,0 +1,18 @@
+package duell.run.main;
+
+
+import duell.objects.Arguments;
+
+import duell.helpers.LogHelper;
+
+class RunMain
+{
+	public static function main()
+	{
+		if (!Arguments.validateArguments())
+        {
+            return;
+        }
+
+	}
+}
