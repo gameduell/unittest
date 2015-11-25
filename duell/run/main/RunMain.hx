7ef2541397ffecf31bfa6e180b770d17e0f564a0
Christian Lührs <christian.luehrs@gameduell.de>
2015-11-25 14:46:34 +0100

    worksave

--- duell/run/main/RunMain.hx
+++ duell/run/main/RunMain.hx
@@ -7,12 +7,33 @@ import duell.helpers.LogHelper;
 
 class RunMain
 {
+	public function new()
+	{
+	}
+
 	public static function main()
 	{
 		if (!Arguments.validateArguments())
         {
             return;
         }
+
+        new RunMain().setupTests();
+	}
+
+	public function setupTests()
+	{
+		specifyTestingPlatform();
+        runTests();
+	}
+
+	private function specifyTestingPlatform()
+	{
+		LogHelper.info("==> " + Arguments.isSet("-path") + " " + Arguments.get("-path"));
+	}
+
+	private function runTests()
+	{
 
 	}
 }
